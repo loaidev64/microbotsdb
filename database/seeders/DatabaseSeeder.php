60f8d4f3b1be5ebf2d791c354717526f6770c9af
loaidev64 <loaidev64@gmail.com>
2024-04-24 10:41:12 +0300

    added project

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,7 +13,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $this->call(AdminSeeder::class);
+        $this->call([
+            AdminSeeder::class,
+            ProjectSeeder::class,
+        ]);
         // User::factory(10)->create();
 
         // User::factory()->create([
